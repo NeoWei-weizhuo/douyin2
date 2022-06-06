002fba247521f3744561eb500fb978e2f098ec2d
neo <UESTCSEWZ@163.com>
2022-06-06 22:17:06 +0800

    第二版

--- douyin-v1.0/main.go
+++ douyin-v1.0/main.go
@@ -6,9 +6,11 @@ import (
 )
 
 func main() {
+	//实例化一个gin对象
 	r := gin.Default()
 
-	initRouter(r)
+	go initRouter(r)
 	conf.Init()
+	conf.Redis("127.0.0.1:6379", "")
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
